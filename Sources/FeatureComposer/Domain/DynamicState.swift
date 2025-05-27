3f3b78d93e54d021f8d861a25d8d502c7f88fab8
Michael Battaglia <michaelbattaglia14@gmail.com>
2025-05-27 14:44:32 -0400

    test hook

--- Sources/FeatureComposer/Domain/DynamicState.swift
+++ Sources/FeatureComposer/Domain/DynamicState.swift
@@ -3,13 +3,13 @@ import Foundation
 
 @dynamicMemberLookup
 public struct DynamicState<State> {
-    private let stream: CurrentValueSubject<State, Never>
-    
-    init(stream: CurrentValueSubject<State, Never>) {
-        self.stream = stream
-    }
-    
-    public subscript<T>(dynamicMember keyPath: KeyPath<State, T>) -> T {
-        stream.value[keyPath: keyPath]
-    }
+  private let stream: CurrentValueSubject<State, Never>
+
+  init(stream: CurrentValueSubject<State, Never>) {
+    self.stream = stream
+  }
+
+  public subscript<T>(dynamicMember keyPath: KeyPath<State, T>) -> T {
+    stream.value[keyPath: keyPath]
+  }
 }
